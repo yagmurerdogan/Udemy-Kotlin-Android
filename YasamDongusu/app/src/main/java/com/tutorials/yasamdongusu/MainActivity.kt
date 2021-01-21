0b47c4b2b346a6b7a2607e6bd6b68801b2c1481a
Yagmur Erdogan <yagmur.erdogan.1999@gmail.com>
2021-01-21 23:20:58 +0300

    First Commit

---
+++ YasamDongusu/app/src/main/java/com/tutorials/yasamdongusu/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.tutorials.yasamdongusu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.e("onCreate","Çalıştı")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e("onStart","Çalıştı")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("onResume","Çalıştı")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("onPause","Çalıştı")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e("onStop","Çalıştı")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("onDestroy","Çalıştı")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.e("onRestart","Çalıştı")
+    }
+}
