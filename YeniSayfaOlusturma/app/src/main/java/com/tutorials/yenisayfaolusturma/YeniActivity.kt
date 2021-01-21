b9154b20aa351dda2f12b8ec2757a82fe3b30c26
Yagmur Erdogan <yagmur.erdogan.1999@gmail.com>
2021-01-21 23:07:01 +0300

    First Commit

---
+++ YeniSayfaOlusturma/app/src/main/java/com/tutorials/yenisayfaolusturma/YeniActivity.kt
@@ -0,0 +1,11 @@
+package com.tutorials.yenisayfaolusturma
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class YeniActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_yeni)
+    }
+}
